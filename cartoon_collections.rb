7bc9ec5534017524070be9a63079c83334f5dd3e
stephen pesce <stevepesce879@gmail.com>
2020-05-02 19:27:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,8 +22,10 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
 
 planeteer_calls.find{|call|
-  valid_calls.each{|valid|
-  (call == valid)
-  }
+  call == valid_calls[0]
+  call == valid_calls[1]
+  call == valid_calls[2]
+  call == valid_calls[3]
+  call == valid_calls[4]
 }
 end
